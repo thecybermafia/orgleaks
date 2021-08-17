dbd43fdc2cbd71d600d6f6b2f155ab5b392e4ed5
Alan Tse <alan.tse@wdc.com>
2021-08-17 16:27:16 -0700

    Match script license to repo license.
    Closes #4

--- orgleaks.sh
+++ orgleaks.sh
@@ -5,16 +5,7 @@
 # Copyright 2021, Rajan Christian
 # Copyright 2021, Western Digital Corporation or its affiliates
   
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License at <http://www.gnu.org/licenses/> for
-# more details.
+# SPDX-License-Identifier: Apache-2.0
 
 # Usage: orgleaks.sh [-h|--help] [-o|--org github] [-v] [-a|--gitleaks-args "--report=org-gitleaks-result --format=csv"]
 
