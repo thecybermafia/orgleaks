eca55a2093cc5d1759f6e430b201412683a9c69c
Rajan Christian <15846532+thecybermafia@users.noreply.github.com>
2021-10-19 12:39:42 +0530

    Fixed the license

--- orgleaks.sh
+++ orgleaks.sh
@@ -4,22 +4,24 @@
 
 # Copyright 2021, Rajan Christian
   
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License at <http://www.gnu.org/licenses/> for
-# more details.
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Usage: orgleaks.sh [-h|--help] [-o|--org github] [-v] [-a|--gitleaks-args "--report=org-gitleaks-result --format=csv"]
 
 # Revision history:
 # 2021-01-21 Initial commit
 # 2021-02-09 Ability to download from github pages and save report
+# 2021-10-19 License Update
 # ---------------------------------------------------------------------------
 
 PROGNAME="orgleaks.sh"
